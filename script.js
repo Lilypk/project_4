19a66ae5a06e9e7da8132adb49ccb5b0eba54545
LILYKLOPCIC <LILYKLOPCIC@GMAIL.COM>
2020-10-13 16:42:46 -0400

    added some variables

---
+++ script.js
@@ -0,0 +1,6 @@
+let leftTopPanel = document.querySelector('.left-top-panel')
+let rightTopPanel = document.querySelector('.right-top-panel')
+let leftBottomPanel = document.querySelector('.left-bottom-panel')
+let rightBottomPanel = document.querySelector('.right-bottom-panel')
+
+let sequences = 
