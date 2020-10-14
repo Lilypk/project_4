d36a942fda83a3b23e7d85a4486387619536e4e3
LILYKLOPCIC <LILYKLOPCIC@GMAIL.COM>
2020-10-13 20:49:10 -0400

    working on flash function

--- script.js
+++ script.js
@@ -2,5 +2,20 @@ let leftTopPanel = document.querySelector('.left-top-panel')
 let rightTopPanel = document.querySelector('.right-top-panel')
 let leftBottomPanel = document.querySelector('.left-bottom-panel')
 let rightBottomPanel = document.querySelector('.right-bottom-panel')
+// let sequence = ['rightBottomPanel', 'leftTopPanel', 'rightTopPanel', 'leftBottomPanel']
 
-let sequences = 
+function getRandomPanel() {
+    let panels = ['rightBottomPanel', 'leftTopPanel', 'rightTopPanel', 'leftBottomPanel']
+    return panels[parseInt(Math.random() * panels.length)]
+}
+getRandomPanel()
+
+let sequence = [getRandomPanel(), getRandomPanel(), getRandomPanel(), getRandomPanel()]
+
+function flash(panel) {
+    let flashPanel = document.createElement('.flashPanel')
+    flashPanel.style.backgroundColor = 'white'
+    
+
+}
+
