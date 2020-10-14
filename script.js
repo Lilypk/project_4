bb3503d6365b457e13726797f8380f15b2de3381
LILYKLOPCIC <LILYKLOPCIC@GMAIL.COM>
2020-10-14 16:44:27 -0400

    ugh

--- script.js
+++ script.js
@@ -1,21 +1,50 @@
+//variables
+let playerSeq = []
+let simonSeq = ['rightBottomPanel', 'leftTopPanel', 'rightTopPanel', 'leftBottomPanel']
 let leftTopPanel = document.querySelector('.left-top-panel')
 let rightTopPanel = document.querySelector('.right-top-panel')
 let leftBottomPanel = document.querySelector('.left-bottom-panel')
 let rightBottomPanel = document.querySelector('.right-bottom-panel')
-// let sequence = ['rightBottomPanel', 'leftTopPanel', 'rightTopPanel', 'leftBottomPanel']
+let level = document.querySelector('.level')
+let panel = document.querySelector('.panel')
 
-function getRandomPanel() {
-    let panels = ['rightBottomPanel', 'leftTopPanel', 'rightTopPanel', 'leftBottomPanel']
-    return panels[parseInt(Math.random() * panels.length)]
+//start board sequence
+function startGame() {
+    let i=0
+    let start = document.querySelector('.start')
+    start.addEventListener('click', function () {
+       level.innerText = (i+=1)
+    })
 }
-getRandomPanel()
+startGame()
 
-let sequence = [getRandomPanel(), getRandomPanel(), getRandomPanel(), getRandomPanel()]
-
-function flash(panel) {
-    let flashPanel = document.createElement('.flashPanel')
-    flashPanel.style.backgroundColor = 'white'
+// simon seq 
+function startSequence() {
+    console.log(level)
     
+    setInterval(function() {
+        let flash = document.querySelectorAll('.panel')
+        
+        // flash[0].classList.toggle('flashPanel')
+        
+    }, 1000)
+
+    getRandomPanel()
 
 }
+startSequence()
+
+// generate random panel
+function getRandomPanel() {
+    let randomPanel = Math.floor(Math.random() * 4)
+    console.log(simonSeq[randomPanel])
+    
+ }
+ getRandomPanel()
+
+
+// user replicates sequence
+
+
+
 
