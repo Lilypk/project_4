91437c48142d1e08b7ac0611d6d5688d32e7a098
LILYKLOPCIC <LILYKLOPCIC@GMAIL.COM>
2020-10-14 19:15:14 -0400

    wed, 7:15

--- script.js
+++ script.js
@@ -1,49 +1,106 @@
 //variables
-let playerSeq = []
-let simonSeq = ['rightBottomPanel', 'leftTopPanel', 'rightTopPanel', 'leftBottomPanel']
+
 let leftTopPanel = document.querySelector('.left-top-panel')
 let rightTopPanel = document.querySelector('.right-top-panel')
 let leftBottomPanel = document.querySelector('.left-bottom-panel')
 let rightBottomPanel = document.querySelector('.right-bottom-panel')
-let level = document.querySelector('.level')
-let panel = document.querySelector('.panel')
+// let level = document.querySelector('.level')
+// let panel = document.querySelector('.panel')
 
-//start board sequence
-function startGame() {
-    let i=0
-    let start = document.querySelector('.start')
-    start.addEventListener('click', function () {
-       level.innerText = (i+=1)
+
+const getRandomPanel = () => {
+
+    const panels = [
+        leftTopPanel,
+        rightTopPanel,
+        rightBottomPanel,
+        leftBottomPanel
+    ]
+    return panels[parseInt(Math.random() * panels.length)]
+}
+
+
+const sequence = [
+    getRandomPanel(),
+    getRandomPanel(),
+    getRandomPanel(),
+    getRandomPanel()
+]
+
+const flash = panel => {
+    return new Promise((resolve, reject) => {
+        panel.className += ' active'
+        setTimeout(() => {
+            panel.className = panel.className.replace(
+                ' active',
+                ''
+            )
+            setTimeout(() => {
+    resolve()
+}, 250)
+        }, 1000)
     })
 }
-startGame()
 
-// simon seq 
-function startSequence() {
-    console.log(level)
-    
-    setInterval(function() {
-        let flash = document.querySelectorAll('.panel')
-        
-        // flash[0].classList.toggle('flashPanel')
-        
-    }, 1000)
+const panelClicked = panel => {
+    console.log(panel)
+}
 
-    getRandomPanel()
+const main = async () => {
+    for (const panel of sequence) {
+        await flash(panel)
+    }
 
 }
-startSequence()
-
-// generate random panel
-function getRandomPanel() {
-    let randomPanel = Math.floor(Math.random() * 4)
-    console.log(simonSeq[randomPanel])
-    
- }
- getRandomPanel()
+main()
 
 
-// user replicates sequence
+
+
+
+
+
+//start board sequence
+// function startGame() {
+//     let i=0
+//     let start = document.querySelector('.start')
+//     start.addEventListener('click', function () {
+//        level.innerText = (i+=1)
+//     })
+// }
+// startGame()
+
+
+
+// // simon seq 
+// function startSequence(sequence) {
+//     console.log(level)
+
+//     setInterval(function() {
+//         let panels= document.querySelectorAll('.panel')
+//         for (let i=0; i < panels.length; i++) {
+//              panels[i].classList.toggle('flashPanel')
+//         }
+
+
+//     }, 1000)
+
+// }
+
+// startSequence()
+
+// // generate random panel
+// function getRandomPanel() {
+//     let randomPanel = Math.floor(Math.random() * 4) 
+//     console.log(simonSeq[randomPanel])
+//     return simonSeq[randomPanel]
+//  }
+//  getRandomPanel()
+
+
+
+
+// // user replicates sequence
 
 
 
